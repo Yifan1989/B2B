b7f243be55fe85ffdbea7ef093b3131ae182e575
Yifan1989 <j.codes1989@gmail.com>
2020-01-18 11:37:17 +1300

    working on service

--- WareHouse_System/WareHouse_System/ClientApp/src/app/models/user.ts
+++ WareHouse_System/WareHouse_System/ClientApp/src/app/models/user.ts
@@ -1,4 +1,7 @@
-﻿export class User {
+﻿/*
+Needs to export the class because it will be imported like services or components.
+*/
+export class User {
     public userId: number;
     public email: string;
     public password: string;
