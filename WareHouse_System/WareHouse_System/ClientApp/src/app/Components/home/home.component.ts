91b39ad58192361eea7c8dc99d088dcbe0a12642
Yifan1989 <j.codes1989@gmail.com>
2020-01-18 00:42:05 +1300

    home

---
+++ WareHouse_System/WareHouse_System/ClientApp/src/app/Components/home/home.component.ts
@@ -0,0 +1,16 @@
+﻿import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+@Component({
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+})
+export class HomeComponent {
+    private loginForm = new FormGroup({
+        userName: new FormControl(''),
+        passWord: new FormControl(''),
+    });
+
+
+
+}
--- WareHouse_System/WareHouse_System/ClientApp/src/app/app.module.ts
+++ WareHouse_System/WareHouse_System/ClientApp/src/app/app.module.ts
@@ -1,11 +1,10 @@
 ﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-//import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +13,6 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-    //HttpClientModule,
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
