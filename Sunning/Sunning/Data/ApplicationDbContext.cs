a1e140f17a8ea9d3b3c82041807ae23938556cea
Yifan1989 <j.codes1989@gmail.com>
2020-02-02 18:53:58 +1300

    db

---
+++ Sunning/Sunning/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Sunning.models;
+
+namespace Sunning.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        //Data querying in Entity Framework Core is performed against the
+        //DbSet properties of the DbContext. The DbSet represents a collection
+        //of entities of a specific type - the type specified by the type parameter.
+
+        // every set is like a table in the db
+        public DbSet<Login> Logins { get; set; }
+    }
+}
---
+++ Sunning/Sunning/models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace Sunning.models
+{
+    public class Login
+    {
+        [Key]
+        public string user { get; set; }
+        public string passwd { get; set; }
+        public Login()
+        {
+        }
+    }
+}
