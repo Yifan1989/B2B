b4ea88332b7adf3d55356077ff015a508c2b1f5a
Yifan1989 <j.codes1989@gmail.com>
2020-02-02 19:24:22 +1300

    db get test done

---
+++ Sunning/Sunning/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Sunning.Data;
+using Sunning.models;
+using System.Threading.Tasks;
+
+namespace Sunning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController: ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        public LoginController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetLogins()
+        {
+            return Ok(_db.Logins.ToList());
+        }
+    }
+}
---
+++ Sunning/Sunning/Migrations/20200202055735_InitialCreate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sunning.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Logins",
+                columns: table => new
+                {
+                    user = table.Column<string>(nullable: false),
+                    passwd = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Logins", x => x.user);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Logins");
+        }
+    }
+}
