513339f145986aa2532a540529a063dd9d729b87
Yifan1989 <j.codes1989@gmail.com>
2020-02-12 14:50:35 +1300

    to service

--- Sunning/Sunning/ClientApp/src/app/components/warehouse/warehouse.component.ts
+++ Sunning/Sunning/ClientApp/src/app/components/warehouse/warehouse.component.ts
@@ -1,5 +1,6 @@
 ﻿import { Component, OnInit, TemplateRef, ViewChild, ElementRef,Output, EventEmitter } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { FormGroup, FormControl, FormBuilder } from '../../../../node_modules/@angular/forms';
 
 @Component({
   selector: 'app-warehouse',
@@ -10,21 +11,29 @@ export class WarehouseComponent implements OnInit {
     @ViewChild('warehouseTemplate')
     public warehouseTemplate: ElementRef;
 
-   
-    //private modalRef: BsModalRef;
     @Output() onHide = new EventEmitter<void>();
 
     // needs to add into provider in app.module.ts file
+
+    private wareHouseForm = new FormGroup({
+        name: new FormControl(''),
+        address: new FormControl(''),
+        owner: new FormControl(''),
+        comment: new FormControl(''),
+    });
+
     constructor(private modalService: BsModalService,
                 public modalRef: BsModalRef) { }
 
     ngOnInit() {
 
     }
-
     
     private doHide(): void {
         this.modalRef.hide();
     }
-    
+
+    private createNewWarehouse(): void {
+
+    }
 }
